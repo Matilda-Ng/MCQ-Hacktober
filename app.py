79bcab937207a34569889bc84a4dd17a8446764f
Ngiam-YiLee <69886757+Ngiam-YiLee@users.noreply.github.com>
2020-10-21 14:45:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ print()
 
 counter = 0
 score = 0
-total_num_of_qn = 3
+total_num_of_qn = 1
 
 
 counter +=1
@@ -11,25 +11,25 @@ tracker = 0
 
 while tracker !=1:
   
-  print("Q"+str(counter)+") "+ "What is 12 + 2?")
-  print("   a) 10")
-  print("   b) 24")
-  print("   c) 14")
-  print("   d) 6")
+  print("Q"+str(counter)+") "+ "What is the solvent in bubble tea?")
+  print("   a) water")
+  print("   b) tea")
+  print("   c) tapioca pearls")
+  print("   d) milk")
   answer = input("Your answer: ")
   answer = answer.lower()
   if answer == "a":
-    output = "Wrong. This is not subtraction."
-    score -=1
-  elif answer == "b":
-    output = "Wrong. This is not multiplication."
-    score -=1
-  elif answer == "c":
-    output = "Yes, that's right!"
+    output = "Correct, you at least have a brain!"
     tracker =1
     score +=1
+  elif answer == "b":
+    output = "Wrong. Tea is made with water and tea leaves."
+    score -=1
+  elif answer == "c":
+    output = "Wrong. Pearls are the solute."
+    score -=1
   elif answer == "d":
-    output = "Wrong. This is not division."
+    output = "Wrong. Milk is made with water and milk powder."
     score -=1
   else:
     output = "Please choose a, b, c or d only."
@@ -40,84 +40,4 @@ while tracker !=1:
   print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
   print()
   print()
-  
-
-
-counter +=1
-tracker = 0
-
-while tracker !=1:
-  
-  print("Q"+str(counter)+") "+ "The chemical formula H2 represents")
-  print("   a) one hydrogen molecule")
-  print("   b) two hydrogen atoms")
-  print("   c) one hydrogen atom")
-  print("   d) two hydrogen molecules")
-  answer = input("Your answer: ")
-  answer = answer.lower()
-  if answer == "a":
-    output = "Yes, that's right!"
-    tracker =1
-    score +=1
-  elif answer == "b":
-    output = "Wrong. If so, then it will be written as H and H - two hydrogen atoms."
-    score -=1
-  elif answer == "c":
-    output = "Wrong. Clearly the number 2 in the formulae must mean something?"
-    score -=1
-    
-  elif answer == "d":
-    output = "Wrong. What's the difference between a molecule and an atom?"
-    score -=1
-  else:
-    output = "Please choose a, b, c or d only."
-
-  print()
-  print(output)
-  print()
-  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
-  print()
-  print()
-  
-  
-
-counter +=1
-tracker = 0
-
-while tracker !=1:
-  
-  print("Q"+str(counter)+") "+ "Which represents the electronic configuration of a non-metal?")
-  print("   a) 2,1")
-  print("   b) 2,8,3")
-  print("   c) 2,8,8,2")
-  print("   d) 2,7")
-  answer = input("Your answer: ")
-  answer = answer.lower()
-  if answer == "a":
-    output = "Wrong. Think again - how many electron shells are filled, and which group is this in?"
-    score -=1
-  elif answer == "b":
-    output = "Wrong.  Think again - how many electron shells are filled, and which group is this in?"
-    score -=1
-  elif answer == "c":
-    output = "Wrong.  Think again - how many electron shells are filled, and which group is this in?"
-    score -=1
-    
-  elif answer == "d":
-    output = "Yes, that's right!"
-    tracker =1
-    score +=1
-  else:
-    output = "Please choose a, b, c or d only."
-
-  
-
-  print()
-  print(output.lower())
-  print()
-  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
-  print()
-  print()
-  
-print("End of quiz!")
   
